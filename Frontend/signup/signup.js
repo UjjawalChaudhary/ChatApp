0ff94ace4a590531e6576a4554568fbf3a0aedcf
HyperZep <ujjwal@hyperzepinfotech.com>
2023-08-22 17:14:33 +0530

    Update 1

--- Frontend/signup/signup.js
+++ Frontend/signup/signup.js
@@ -9,7 +9,7 @@ async function signup(e){
         password:e.target.password.value
         
        }
-       const response=await axios.post("http://54.145.5.97:3000/user/signup",signupDetails)
+       const response=await axios.post("http://localhost:3000/user/signup",signupDetails)
        .then(response=>{
         console.log(response)
         if(response.status ===201){
